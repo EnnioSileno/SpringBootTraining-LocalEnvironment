87ff3562ddf3ab50826e0456cad471f567b4f2af
Ennio Sileno <silenenn@students.zhaw.ch>
2022-10-17 20:06:34 +0200

    created StudentConfig
    Preconfiguration for start of the rest server added

---
+++ src/main/java/ch/ennio/sileno/sqltest/student/StudentConfig.java
@@ -0,0 +1,35 @@
+package ch.ennio.sileno.sqltest.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static javax.xml.datatype.DatatypeConstants.*;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student ennio = new Student(1L,
+                    "Ennio Sileno",
+                    "esileno@gmail.com",
+                    LocalDate.of(1997, AUGUST, 10),
+                    25
+            );
+            Student robin = new Student(2L,
+                    "Robin Sileno",
+                    "robin@gmail.com",
+                    LocalDate.of(1999, AUGUST, 27),
+                    23
+            );
+            repository.saveAll(
+                    List.of(ennio, robin)
+            );
+        };
+    }
+}
