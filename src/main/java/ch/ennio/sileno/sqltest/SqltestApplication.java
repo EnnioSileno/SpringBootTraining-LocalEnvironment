223ad686e25b5c050fbd2adab366d28e13bf4550
Ennio Sileno <silenenn@students.zhaw.ch>
2022-10-17 16:37:49 +0200

    init project

---
+++ src/main/java/ch/ennio/sileno/sqltest/SqltestApplication.java
@@ -0,0 +1,13 @@
+package ch.ennio.sileno.sqltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqltestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqltestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/ennio/sileno/sqltest/SqltestApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.ennio.sileno.sqltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqltestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
